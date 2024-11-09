2b45abcb8114ca27f55d55e40f4bff08e43f0784
Ezra Blasko <reynacalterion@gmail.com>
2024-11-08 19:07:27 -0500

    Cleaned up docs, renamed reference tracking to ownership to represent what I'm actually doing

--- src/lib.rs
+++ src/lib.rs
@@ -3,74 +3,75 @@
 //! 
 //! This is a learning experience for me and should be used with a mountain of salt.
 //! 
-//! Allocates an area of memory using Rust's [Vec], storing fixed size blocks of data (until I learn to handle raw memory).
-//! This is a rather low level library, handling garbage collection semi-manually (see *link to reference stuff*) and intended to be integrated into larger projects, specifically datastructures like trees where Rust's lifetime system makes things difficult. You will not be protected from memory leaks, though leakage can be mitigated with calls to [MemHeap::remove_memory_leaks].
+//! Effectively a custom implementation of [std::rc] with a focus on streamlining the creation of a large number of shared-ownership data and ensuring all of that data is stored (more or less) contiguously in memory. Data is stored in a [Vec] (until I learn how to handle raw memory), and [Index]es are used to read and write.
+//! 
+//! Insert an example here: (Note to self)
 
 use std::ops::Deref;
 
 /// A collection of errors which may occur while handling memory.
 #[derive(Debug)]
 pub enum AccessError {
-    /// Returned when attempting to access an Index beyond the length of [MemHeap]'s internal storage
+    /// Returned when attempting to access an index beyond the length of [MemHeap]'s internal storage
     OutOfBoundsMemory(usize),
-    /// Returned when attempting to access an Index marked as Protected
+    /// Returned when attempting to access an index marked as protected
     ProtectedMemory(usize),
-    /// Returned when attempting to access an Index which isn't currently allocated
+    /// Returned when attempting to access an index which isn't currently allocated
     FreeMemory(usize),
     /// Returned when attempting to do something which isn't supported
     InvalidRequest,
 }
 
 
-mod reference_management {
+mod owner_tracking {
 
     #[derive(PartialEq)]
-    /// The current status of the MemHeap's reference tracking for a piece of data
-    pub enum ReferenceStatus {
-        /// There are `usize` references to the data
+    /// The current status of data ownership
+    pub enum Ownership {
+        /// There are `usize` owners of the data
         Fine(usize),
-        /// There are no references to the data, it's dangling and should be freed.
+        /// Nobody owns the data, it's dangling and should be freed.
         Dangling,
     }
     
-    pub struct ReferenceWrapper<T> {
+    pub struct Steward<T> {
         pub data : T,
-        ref_count:usize,
+        owner_count:usize,
     }
     
-    impl<T> ReferenceWrapper<T> {
+    impl<T> Steward<T> {
     
         pub fn new(data:T) -> Self {
             Self {
                 data,
-                ref_count : 0,
+                owner_count : 0,
             }
         }
     
-        pub fn modify_ref(&mut self, delta:isize) -> Result<ReferenceStatus, super::AccessError> {
+        pub fn modify_owners(&mut self, delta:isize) -> Result<Ownership, super::AccessError> {
             if delta < 0 {
-                match self.ref_count.checked_sub(delta.abs() as usize) {
+                match self.owner_count.checked_sub(delta.abs() as usize) {
                     Some(zero) if zero == 0 => {
-                        self.ref_count = 0;
-                        Result::Ok(ReferenceStatus::Dangling)
+                        self.owner_count = 0;
+                        Result::Ok(Ownership::Dangling)
                     },
                     Some(new_ref) => {
-                        self.ref_count = new_ref;
-                        Result::Ok(ReferenceStatus::Fine(self.ref_count))
+                        self.owner_count = new_ref;
+                        Result::Ok(Ownership::Fine(self.owner_count))
                     },
                     None => Result::Err( super::AccessError::InvalidRequest )
                 }     
             } else {
-                self.ref_count += delta as usize;
-                Result::Ok(ReferenceStatus::Fine(self.ref_count))
+                self.owner_count += delta as usize;
+                Result::Ok(Ownership::Fine(self.owner_count))
             }
         }
     
-        pub fn status(&self) -> ReferenceStatus {
-            if self.ref_count == 0 {
-                ReferenceStatus::Dangling
+        pub fn status(&self) -> Ownership {
+            if self.owner_count == 0 {
+                Ownership::Dangling
             } else {
-                ReferenceStatus::Fine(self.ref_count)
+                Ownership::Fine(self.owner_count)
             }
         }
     
@@ -89,13 +90,13 @@ impl Deref for Index {
 }
 
 
-pub use reference_management::ReferenceStatus;
-use reference_management::ReferenceWrapper;
+pub use owner_tracking::Ownership;
+use owner_tracking::Steward;
 
 /// Used to allocate space on the stack, read from that space, and write to it.
 pub struct MemHeap<T:Clone> {
     /// The container used to manage allocated memory
-    memory:Vec< Option< ReferenceWrapper< T > > >,
+    memory:Vec< Option< Steward< T > > >,
     /// Stores list of indexes which can be written to 
     free_indexes : Vec<Index>,
     /// Stores list of indexes which can't be referenced mutably
@@ -108,6 +109,7 @@ impl<T:Clone> MemHeap<T> {
     /// # Examples
     /// `
     /// //Stores u32's in each index.
+    /// 
     /// let mut mem_heap:MemHeap<u32> = MemHeap::new();
     /// `
     pub fn new() -> Self {
@@ -130,7 +132,7 @@ impl<T:Clone> MemHeap<T> {
         self.protected_indexes.contains(&index)
     }
 
-    fn mut_wrapper(&mut self, index:Index) -> Result<&mut ReferenceWrapper<T>, AccessError> {
+    fn mut_wrapper(&mut self, index:Index) -> Result<&mut Steward<T>, AccessError> {
         match index {
             bad_index if index >= self.upper_bound() => Err( AccessError::OutOfBoundsMemory(*bad_index) ),
             protected_index if self.is_protected(protected_index) => Err( AccessError::ProtectedMemory(*protected_index) ),
@@ -141,7 +143,7 @@ impl<T:Clone> MemHeap<T> {
         }
     }
 
-    fn wrapper(&self, index:Index) -> Result<&ReferenceWrapper<T>, AccessError> {
+    fn wrapper(&self, index:Index) -> Result<&Steward<T>, AccessError> {
         match index {
             bad_index if index >= self.upper_bound() => Err( AccessError::OutOfBoundsMemory(*bad_index) ),
             index => match &self.memory[*index] {
@@ -188,9 +190,9 @@ impl<T:Clone> MemHeap<T> {
         self.memory.len()
     }
 
-    /// Removes protection from an index, doing nothing if the index isn't protected.
-    /// This should be used when you need to mutate the data at that index for some reason.
-    /// If this is a permanent exposure (you don't plan to call [MemHeap::protect] afterwards), ensure you use [MemHeap::add_ref] or [MemHeap::free_if_dangling] to prevent memory leaking.
+    /// Removes protection from a piece of data, doing nothing if it isn't protected.
+    /// 
+    /// If this is a permanent exposure (you don't plan to call [MemHeap::protect] afterwards), ensure you use [MemHeap::add_owner] or [MemHeap::free_if_dangling] to prevent memory leaking.
     pub fn expose(&mut self, index:Index) {
         if self.is_protected(index) {
             for protected in 0 .. self.protected_indexes.len() {
@@ -202,12 +204,14 @@ impl<T:Clone> MemHeap<T> {
         }
     }
 
-    /// Protects an index, ensuring it's reference_tracker can't be modified/it can't be garbage collected by:
-    /// - [MemHeap::add_ref]
-    /// - [MemHeap::remove_ref]
+    /// Protects a piece of data, ensuring it's ownership tracking won't be altered and the data won't be garbage collected by any of the following:
+    /// - [MemHeap::add_owner]
+    /// - [MemHeap::remove_owner]
     /// - [MemHeap::remove_memory_leaks]
     /// - [MemHeap::free_if_dangling]
-    /// For now free indexes (non-reserved) cannot be protected. This is subject to change as I feel like it.
+    /// - [MemHeap::replace]
+    ///
+    ///  For now free indexes (non-reserved) cannot be protected. This is subject to change as I feel like it.
     pub fn protect(&mut self, index:Index) -> Result<(), AccessError> {
         //We don't protect free indexes. Reserving indexes should be done with reserve_index/reserve_protected
         _ = self.wrapper(index)?;
@@ -215,15 +219,16 @@ impl<T:Clone> MemHeap<T> {
         Ok(())
     }
 
-    /// Frees every index which meets the following requirements:
-    /// - Has zero references to it
+    /// Frees all data which meets the following requirements:
+    /// - Has no owners
     /// - Is not protected
+    /// 
     /// This operation is O(n) to the total number of allocated indexes (which can be found using [MemHeap::length]).
     pub fn remove_memory_leaks(&mut self) {
         for cell in 0 .. self.memory.len() {
             let index = Index(cell);
             if let Ok(wrapper) = self.mut_wrapper(index) {
-                if let ReferenceStatus::Dangling = wrapper.status() {
+                if let Ownership::Dangling = wrapper.status() {
                     self.free_index(index);
                 }
             }
@@ -231,32 +236,40 @@ impl<T:Clone> MemHeap<T> {
     }
 
     /// Returns an immutable reference to the data stored at the requested index, or an [AccessError] if there is a problem.
+    /// 
+    /// The equivalent to using & to borrow variables in normal Rust.
     pub fn data(&self, index:Index) -> Result<&T, AccessError> {
         Ok(&self.wrapper(index)?.data)
     }
 
-    /// Tells the MemHeap that something is referencing the data at `index`.
-    /// So long as MemHeap thinks there is at least one reference to the data, it will not be garbage collected.
-    /// Due to the nature of Rust's lifetime system the MemHeap cannot verify the data's reference count, it will trust you completely.
-    /// If you want to store data regardless of whether something is currently referencing it, please use:
+    /// Tells the MemHeap that something else owns the data at `index`.
+    /// So long as MemHeap thinks there is at least one owner, the data won't be garbage collected.
+    /// 
+    /// Failure to properly track ownership will lead to either garbage collection of active data or leaking of inactive data
+    /// 
+    /// If you want to store data regardless of whether something is currently owning it, please use:
     /// - [MemHeap::protect] for existing data
     /// - [MemHeap::push()] with `protected = true` for new data
-    pub fn add_ref(&mut self, index:Index) -> Result<(), AccessError> {
-        self.mut_wrapper(index)?.modify_ref(1)?;
+    pub fn add_owner(&mut self, index:Index) -> Result<(), AccessError> {
+        self.mut_wrapper(index)?.modify_owners(1)?;
         Ok(())
     }
 
-    /// Tells the MemHeap that something which was referencing the data at `index` isn't anymore.
-    /// MemHeap cannot verify this and will trust you. If the MemHeap thinks there is nothing else referencing the data after removal, it will be garbage collected.
-    /// If you want to store data regardless of whether something is currently referencing it, please use:
+    /// Tells the MemHeap that something no longer owns the data at `index`.
+    /// By default, if calling this function renders the ownercount of data 0, it will automatically be garbage collected and returned.
+    /// 
+    /// Failure to properly track ownership will lead to either garbage collection of active data or leaking of inactive data.
+    /// 
+    /// If you want to store data regardless of whether something owns it, please use:
     /// - [MemHeap::protect] for existing data
     /// - [MemHeap::push] with `protected` set to true for new data
-    /// If instead you're trying to garbage collect protected data, please:
+    /// 
+    /// If instead you're trying to free protected data, please:
     /// 1. Remove the data's protection using [MemHeap::expose]
-    /// 2. Remove any existing references using [MemHeap::remove_ref] (can check if needed with [MemHeap::status])
+    /// 2. Remove any existing references using [MemHeap::remove_owner] (can check if needed with [MemHeap::status])
     /// 3. Free the data using [MemHeap::free_if_dangling]
-    pub fn remove_ref(&mut self, index:Index) -> Result<Option<T>, AccessError> {
-        if let ReferenceStatus::Dangling = self.mut_wrapper(index)?.modify_ref(-1)? {
+    pub fn remove_owner(&mut self, index:Index) -> Result<Option<T>, AccessError> {
+        if let Ownership::Dangling = self.mut_wrapper(index)?.modify_owners(-1)? {
             match self.free_index(index) {
                 Some(data) => Ok( Some(data) ),
                 None => Ok(None)
@@ -264,38 +277,44 @@ impl<T:Clone> MemHeap<T> {
         } else { Ok(None) }
     }
 
-    /// Frees the `index` and returns the data stored there wrapped in an [Option::Some] wrapped in a [Result::Ok] if the MemHeap doesn't know about any references to the data.
-    /// If there are still references, [Option::None] will be returned in the [Result::Ok] instead.
-    /// If the index is invalid, or cannot be freed for some reason, returns an [AccessError].
+    /// Frees the data at `index` and returns it wrapped in an [Option::Some] wrapped in a [Result::Ok] if the data is ownerless.
+    /// If there are still owners, [Option::None] will be returned in the [Result::Ok] instead.
+    /// If the index is invalid, or the data cannot be freed for some reason, returns an [AccessError].
     pub fn free_if_dangling(&mut self, index:Index) -> Result<Option<T>, AccessError> {
         match self.status(index)? {
-            ReferenceStatus::Fine(_) => Ok(None),
-            ReferenceStatus::Dangling => Ok(self.free_index(index)),
+            Ownership::Fine(_) => Ok(None),
+            Ownership::Dangling => Ok(self.free_index(index)),
         }
     }
 
-    /// Returns the [reference_management::ReferenceStatus] of the Index, or an [AccessError] if the request has a problem
-    pub fn status(&self, index:Index) -> Result<reference_management::ReferenceStatus, AccessError> {
+    /// Returns the [owner_tracking::Ownership] of the data at `index`, or an [AccessError] if the request has a problem
+    pub fn status(&self, index:Index) -> Result<owner_tracking::Ownership, AccessError> {
         Ok(self.wrapper(index)?.status())
     }
 
     /// Pushes `data` into the MemHeap, selecting the most *recently freed index for insertion and returning the index the data is placed at.
+    /// 
     /// *Subject to change. In the future the plan is to return the first free index, sequentially, to leave less holes in reserved memory.
-    /// If `protected` is true, the data will be marked as immutable. This means the MemHeap will 'freeze' the data's reference count when calling [MemHeap::add_ref] or [MemHeap::remove_ref] and won't garbage collect the data, even if it's [reference_management::ReferenceStatus::Dangling]
-    /// Protection can be removed with a call to [MemHeap::expose], which will unfreeze the data's reference count.
-    /// If you don't intend to re[MemHeap::protect] the data, please either garbage collect with [MemHeap::free_if_dangling] or give it a reference with [MemHeap::add_ref].
-    /// Remember: **References should correlate to locations an [Index] is stored**. DO NOT just call [MemHeap::add_ref] and forget about it, unless you want to deal with memory leakage.
-    /// Once you recieve the index the data was stored at, it is your responsibility to manage references to it, the MemHeap can't hold your hand here.
+    /// 
+    /// If `protected` is true, the data will be marked as immutable. 
+    /// This means the MemHeap will 'freeze' the data's ownership tracking when calling [MemHeap::add_owner] or [MemHeap::remove_owner] and won't garbage collect the data, even if it is [owner_tracking::Ownership::Dangling]
+    /// 
+    /// Protection can be removed with a call to [MemHeap::expose], which will unfreeze the data's ownership tracking.
+    /// If you don't intend to re[MemHeap::protect] the data, please either garbage collect with [MemHeap::free_if_dangling] or give it an owner with [MemHeap::add_owner].
+    /// Remember: **Owners should correlate to locations an [Index] is stored**. DO NOT just call [MemHeap::add_owner] and forget about it unless you want to deal with memory leakage.
+    /// 
+    /// Once you recieve the index the data was stored at, it is your responsibility to manage it's owners.
     pub fn push(&mut self, data:T, protected:bool) -> Index {
         let index = if protected { self.reserve_protected() } else { self.reserve_index() };
-        self.memory[*index] = Some( ReferenceWrapper::new(data) );
+        self.memory[*index] = Some( Steward::new(data) );
         index
     }
 
     /// Replaces the data at `index` with `new_data`, returning the replaced data on success and an [AccessError] on failure.
     /// You may only replaced reserved, non-protected data. Free indexes should be filled with [MemHeap::push].
+    /// 
     /// If you want to replace protected data:
-    /// 1. Call [MemHeap::expose] on the index to remove its protection
+    /// 1. Call [MemHeap::expose] on the data's index to remove its protection
     /// 2. Call [MemHeap::replace] to replace the data
     /// 3. Call [MemHeap::protect] to restore its protection (if desired)
     pub fn replace(&mut self, index:Index, new_data:T) -> Result<T, AccessError> {
