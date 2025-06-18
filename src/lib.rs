8b0d3efd8a4f29885cdd673cf5c92e96744aa662
Karanstr <139508165+Karanstr@users.noreply.github.com>
2025-06-18 16:46:45 -0400

    Fixed doc tests and removed the Indexable trait, everything is just usize now

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,6 @@
 //! 
 //! Errors will cancel the request and returning an [AccessError].
 //! 
-//! Indexing is performed internally using [usize], but you can use any type which implements [Indexable].
-//! 
 //! # Example
 //! ```
 //! use vec_mem_heap::prelude::*;
@@ -39,9 +37,9 @@
 //!     }
 //! 
 //!     // returns Ok( &15 ) -> The data at index 0 (data1) still has one reference.
-//!     dbg!( storage.data( data1 ) );
+//!     dbg!( storage.get( data1 ) );
 //!     // Err( AccessError::FreeMemory(1) ) -> The data at index 1 was freed when its last reference was removed.
-//!     dbg!( storage.data( 1 ) );
+//!     dbg!( storage.get( 1 ) );
 //! 
 //! }
 //! ```
@@ -56,27 +54,15 @@ use std::collections::HashMap;
 pub mod prelude {
   pub use super::{
     NodeField, 
-    Indexable,
     AccessError
   };
 }
-
-/// A trait which allows you to customize how indexes are stored on your side of the api
-pub trait Indexable {
-  /// Allows the library to convert your type to its internal [Index] representation (currently [usize])
-  fn to_index(&self) -> Index;
-}
-type Index = usize;
-impl Indexable for usize {
-  fn to_index(&self) -> Index { *self }
-}
-
 
 /// Errors which may occur while accessing and modifying memory.
 #[derive(Debug)]
 pub enum AccessError {
   /// Returned when attempting to access an index which isn't currently allocated
-  FreeMemory(Index),
+  FreeMemory(usize),
   /// Returned when a reference operation causes an over/underflow
   ReferenceOverflow,
 }
@@ -92,26 +78,20 @@ pub struct NodeField<T:Clone> {
 
 // Private methods
 impl<T:Clone> NodeField<T> {
-  fn last_index(&self) -> Index {
-    self.data.len() - 1
-  }
+  fn last_index(&self) -> usize { self.data.len() - 1 }
 
-  fn first_free(&self) -> Option<Index> {
+  fn first_free(&self) -> Option<usize> {
     for (index, reference) in self.refs.iter().enumerate() {
       if reference.is_none() { return Some(index) }
     }
     None
   }
 
-  fn mark_free(&mut self, index:Index) {
-    self.refs[index] = None;
-  }
+  fn mark_free(&mut self, idx:usize) { self.refs[idx] = None; }
 
-  fn mark_reserved(&mut self, index:Index) {
-    self.refs[index] = Some(0);
-  }
+  fn mark_reserved(&mut self, idx:usize) { self.refs[idx] = Some(0); }
 
-  fn release(&mut self, index:Index) -> T {
+  fn release(&mut self, index:usize) -> T {
     if let Some(data) = self.data[index].take() {
       self.mark_free(index);
       data
@@ -119,17 +99,14 @@ impl<T:Clone> NodeField<T> {
   }
 
   #[must_use]
-  fn reserve(&mut self) -> Index {
-    let index = match self.first_free() {
-      Some(index) => index,
-      None => {
-        self.data.push(None);
-        self.refs.push(None);
-        self.last_index()
-      }
+  fn reserve(&mut self) -> usize {
+    let idx = if let Some(idx) = self.first_free() { idx } else {
+      self.data.push(None);
+      self.refs.push(None);
+      self.last_index()
     };
-    self.mark_reserved(index);
-    index
+    self.mark_reserved(idx);
+    idx
   }
 
 }
@@ -139,6 +116,7 @@ impl<T:Clone> NodeField<T> {
   /// Constructs a new `NodeField` which can store data of type `T` 
   /// # Example
   /// ```
+  /// use vec_mem_heap::prelude::*;
   /// //Stores i32s
   /// let mut storage = NodeField::<i32>::new();
   /// ```
@@ -150,36 +128,36 @@ impl<T:Clone> NodeField<T> {
   }
 
   /// Returns an immutable reference to the data stored at the requested index, or an [AccessError] if there is a problem.
-  pub fn get<I:Indexable>(&self, index:I) -> Result<&T, AccessError> {
-    if let Some(data) = self.data.get(index.to_index()) {
+  pub fn get(&self, idx:usize) -> Result<&T, AccessError> {
+    if let Some(data) = self.data.get(idx) {
       Ok(data.as_ref().unwrap())
-    } else { Err(AccessError::FreeMemory(index.to_index())) }
+    } else { Err(AccessError::FreeMemory(idx)) }
   }
 
   /// Returns a mutable reference to the data stored at the requested index, or an [AccessError] if there is a problem.
-  pub fn get_mut<I:Indexable>(&mut self, index:I) -> Result<&mut T, AccessError> {
-    if let Some(data) = self.data.get_mut(index.to_index()) {
+  pub fn get_mut(&mut self, idx:usize) -> Result<&mut T, AccessError> {
+    if let Some(data) = self.data.get_mut(idx) {
       Ok(data.as_mut().unwrap())
-    } else { Err(AccessError::FreeMemory(index.to_index())) }
+    } else { Err(AccessError::FreeMemory(idx)) }
   }
 
   /// Tells the NodeField that something else references the data at `index`.
   /// So long as the NodeField thinks there is at least one reference, the data won't be freed.
   /// 
   /// Failure to properly track references will lead to either freeing data you wanted or leaking data you didn't.
-  pub fn add_ref<I:Indexable>(&mut self, index:I) -> Result<(), AccessError> {
-    if let Some(Some(count)) = self.refs.get_mut(index.to_index()) {
+  pub fn add_ref(&mut self, idx:usize) -> Result<(), AccessError> {
+    if let Some(Some(count)) = self.refs.get_mut(idx) {
       *count = count.checked_add(1).ok_or(AccessError::ReferenceOverflow)?;
       Ok(())
-    } else { Err(AccessError::FreeMemory(index.to_index())) }
+    } else { Err(AccessError::FreeMemory(idx)) }
   }
 
   /// Tells the NodeField that something no longer references the data at `index`.
   /// If calling this function renders the refcount 0 the data will be freed and returned.
   /// 
   /// Failure to properly track references will lead to either freeing data you wanted or leaking data you didn't.
-  pub fn remove_ref<I:Indexable>(&mut self, index:I) -> Result<Option<T>, AccessError> {
-    let internal_index = index.to_index();
+  pub fn remove_ref(&mut self, idx:usize) -> Result<Option<T>, AccessError> {
+    let internal_index = idx;
     if let Some(Some(count)) = self.refs.get_mut(internal_index) {
       *count = count.checked_sub(1).ok_or(AccessError::ReferenceOverflow)?;
       if *count == 0 { Ok( Some( self.release(internal_index) ) ) } else { Ok(None) }
@@ -187,10 +165,10 @@ impl<T:Clone> NodeField<T> {
   }
 
   /// Returns the number of references the data at `index` has or an [AccessError] if there is a problem.
-  pub fn status<I:Indexable>(&self, index:I) -> Result<usize, AccessError> {
-    if let Some(Some(count)) = self.refs.get(index.to_index()) {
+  pub fn status(&self, idx:usize) -> Result<usize, AccessError> {
+    if let Some(Some(count)) = self.refs.get(idx) {
       Ok(*count)
-    } else { Err(AccessError::FreeMemory(index.to_index())) }
+    } else { Err(AccessError::FreeMemory(idx)) }
   }
 
   /// Pushes `data` into the NodeField, returning the index it was stored at.
@@ -198,25 +176,23 @@ impl<T:Clone> NodeField<T> {
   /// Once you recieve the index the data was stored at, it is your responsibility to manage its references.
   /// The data will start with one reference.
   #[must_use]
-  pub fn push(&mut self, data:T) -> Index {
-    let index = self.reserve();
-    self.data[index] = Some(data);
-    self.add_ref(index).unwrap();
-    index
+  pub fn push(&mut self, data:T) -> usize {
+    let idx = self.reserve();
+    self.data[idx] = Some(data);
+    self.add_ref(idx).unwrap();
+    idx
   }
 
   /// Replaces the data at `index` with `new_data`, returning the original data on success and an [AccessError] on failure.
   /// You may not replace an index which is currently free. 
-  pub fn replace<I:Indexable>(&mut self, index:I, new_data:T) -> Result<T, AccessError> {
-    if let Some(Some(_)) = self.refs.get(index.to_index()) {
-      Ok(self.data[index.to_index()].replace(new_data).unwrap())
-    } else { Err(AccessError::FreeMemory(index.to_index())) }
+  pub fn replace(&mut self, idx:usize, new_data:T) -> Result<T, AccessError> {
+    if let Some(Some(_)) = self.refs.get(idx) {
+      Ok(self.data[idx].replace(new_data).unwrap())
+    } else { Err(AccessError::FreeMemory(idx)) }
   }
 
   /// Returns the next index which will be allocated on a [NodeField::push] call
-  pub fn next_allocated(&self) -> Index { 
-    self.first_free().unwrap_or(self.data.len())
-  }
+  pub fn next_allocated(&self) -> usize { self.first_free().unwrap_or(self.data.len()) }
 
   /// Travels through memory and re-arranges slots so that they are contiguous in memory, with no free slots in between occupied ones.
   /// The hashmap returned can be used to remap your references to their new locations. (Key:Old, Value:New)
@@ -225,7 +201,7 @@ impl<T:Clone> NodeField<T> {
   /// 
   /// This operation is O(n) to the number of slots in memory.
   #[must_use]
-  pub fn defrag(&mut self) -> HashMap<Index, Index> {
+  pub fn defrag(&mut self) -> HashMap<usize, usize> {
     let mut remapped = HashMap::new();
     let mut solid_until = 0;
     if solid_until == self.data.len() { return remapped }
@@ -248,7 +224,7 @@ impl<T:Clone> NodeField<T> {
 
   /// [NodeField::defrag]s the memory, then shrinks the internal memory Vec to the size of the block of occupied memory.
   #[must_use]
-  pub fn trim(&mut self) -> HashMap<Index, Index> {
+  pub fn trim(&mut self) -> HashMap<usize, usize> {
     let remap = self.defrag();
     if let Some(first_free) = self.first_free() {
       self.data.truncate(first_free);
@@ -260,12 +236,8 @@ impl<T:Clone> NodeField<T> {
   }
 
   /// Returns a reference to the internal data Vec
-  pub fn data(&self) -> &Vec< Option< T > > {
-    &self.data
-  }
+  pub fn data(&self) -> &Vec< Option< T > > { &self.data }
 
   /// Returns a reference to the internal reference Vec
-  pub fn refs(&self) -> &Vec< Option< usize > > {
-    &self.refs
-  }
+  pub fn refs(&self) -> &Vec< Option< usize > > { &self.refs }
 }
