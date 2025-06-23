658f10eca29afe6517896c6038e394d386c5e094
Karanstr <139508165+Karanstr@users.noreply.github.com>
2025-06-23 16:35:08 -0400

    Docs completed, say hello to our new library :party:

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,69 @@
-#[deny(missing_docs)]
+#![warn(missing_docs)]
+//! Fun little object pool allocator.
+//!
+//! This is a learning experience for me, but I'm farely confident it doesn't suck that badly.
+//!
+//! This crate was originally intended for the creation of tree-like datastructures,
+//! where indexes could be used instead of dealing with rust's reference/pointer system.
+//! The vision of the project has somewhat shifted since v0.8 and now is intended as a 
+//! general purpose object pool, for whatever you need to be pooling. It attempts to keep data as
+//! contiguous as possible, [Pond::alloc] reserves the first free node and [Pond::defrag] +
+//! [Pond::trim] are provided to maintain contiguity on otherwise sparse data.
+//!
+//! This crate isn't yet thread safe, but that's eventually on the todo list probably.
+//!
+//! # Example
+//! ```
+//! use lilypads::Pond;
+//!
+//! fn main() {
+//!   let mut pool = Pond::new();
+//!   // You can push data into the pond and recieve their index.
+//!   let idx1 = pool.alloc(57);
+//!   let idx2 = pool.alloc(42);
+//!
+//!   // Data is retrieved with get
+//!   let data1 = pool.get(idx1).unwrap();
+//!   assert_eq!(*data1, 57);
+//!   // And get_mut
+//!   let data2 = pool.get_mut(idx2).unwrap();
+//!   *data2 = 13;
+//!   assert_eq!(*pool.get(idx2).unwrap(), 13);
+//!
+//!   // Data can be freed with free, which will return the data stored at the index.
+//!   let freed1 = pool.free(idx1).unwrap();
+//!   assert_eq!(freed1, 57);
+//!   assert_eq!(pool.get_mut(idx1), None);
+//!
+//!   // You can request a specific index with write, overwriting the existing data 
+//!   // and returning whatever used to be there
+//!   let replaced = pool.write(idx2, 98);
+//!   assert_eq!(*pool.get(idx2).unwrap(), 98);
+//!
+//!   let far_idx = 17;
+//!   let nothing = pool.write(far_idx, 1000);
+//!   assert_eq!(nothing, None);
+//!   assert_eq!(*pool.get(far_idx).unwrap(), 1000);
+//!   
+//! }
+//! ```
+
 mod binary_tree;
 use binary_tree::BinaryTree;
 use std::collections::HashMap;
 use std::mem::MaybeUninit;
 
+/// The struct used to pool T.
+///
+/// The first avaliable node will be allocated when you call [Pond::alloc],
+/// intending to keep the data as contiguous as possible. If you need total contiguity,
+/// [Pond::defrag] and [Pond::trim] should help with that.
 #[derive(Debug)]
-pub struct NodePool<T> {
+pub struct Pond<T> {
   data : Vec< MaybeUninit<T> >,
   list: BinaryTree,
 }
-// Private methods
-impl<T> NodePool<T> {
+impl<T> Pond<T> {
 
   fn is_reserved(&self, idx: usize) -> bool {
     match self.list.is_full(idx) {
@@ -39,8 +92,8 @@ impl<T> NodePool<T> {
   }
 
 }
-// Public functions
-impl<T> NodePool<T> {
+impl<T> Pond<T> {
+  /// Creates a new instance of [Pond]
   pub fn new() -> Self {
     Self {
       data : Vec::new(),
@@ -48,14 +101,14 @@ impl<T> NodePool<T> {
     }
   }
 
+  /// Returns the number of slots held internally, both free and full.
   pub fn len(&self) -> usize { self.data.len() }
 
-  /// Returns the next index which will be allocated on a [NodePool::alloc] call. If you need to
-  /// guarantee a certain value, use [NodePool::write] instead.
+  /// Returns the next index which will be allocated on a [Pond::alloc] call. If you need to
+  /// guarantee a certain value, use [Pond::write] instead.
   pub fn next_allocated(&self) -> usize { self.first_free().unwrap_or(self.data.len()) }
 
-  /// Sets NodePool to hold `size` elements. If size < self.data().len(), excess data will be truncated
-  /// and dropped. Use care when calling this function.
+  /// Sets Pond to hold `size` elements. If size < self.data().len(), excess data will be truncated and dropped.
   pub fn resize(&mut self, size: usize) {
     let additional = size.saturating_sub(self.data.len());
     while let Some(idx) = self.list.find_last_full() {
@@ -92,7 +145,7 @@ impl<T> NodePool<T> {
   /// Overwrite and reserve the data at `idx`. 
   /// Returns Some(old_data) or None, depending whether the slot was previously reserved.
   ///
-  /// This function will [NodePool::resize] if `idx` is beyond [NodePool::len], guaranteeing
+  /// This function will [Pond::resize] if `idx` is beyond [Pond::len], guaranteeing
   /// your data will be written to the requested slot.
   pub fn write(&mut self, idx:usize, new_data:T) -> Option<T> {
     if idx >= self.len() { self.resize(idx + 1) }
@@ -139,7 +192,7 @@ impl<T> NodePool<T> {
     remapped
   }
 
-  /// [NodePool::defrag]s the memory, then shrinks the internal vec to fit remaining data.
+  /// [Pond::defrag]s the memory, then shrinks the internal vec to fit remaining data.
   #[must_use]
   pub fn trim(&mut self) -> HashMap<usize, usize> {
     let remap = self.defrag();
@@ -159,16 +212,16 @@ impl<T> NodePool<T> {
   pub fn unsafe_data(&self) -> &Vec<MaybeUninit<T>> { &self.data }
 }
 
+
 use serde::{Serialize, Serializer, ser::SerializeSeq, Deserialize, Deserializer};
-impl<T> Serialize for NodePool<T> where T: Serialize {
+impl<T> Serialize for Pond<T> where T: Serialize {
   fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
     let mut seq = serializer.serialize_seq(Some(self.data.len()))?;
     for idx in 0 .. self.data.len() { seq.serialize_element(&self.get(idx))?; }
     seq.end()
   }
 }
-
-impl<'de, T> Deserialize<'de> for NodePool<T> where T: Deserialize<'de> {
+impl<'de, T> Deserialize<'de> for Pond<T> where T: Deserialize<'de> {
   fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
     let data: Vec<Option<T>> = Vec::deserialize(deserializer)?;
     let mut pool = Self::new();
--- tests/node_field_tests.rs
+++ tests/node_field_tests.rs
@@ -1,165 +1,174 @@
-use vec_mem_heap::NodePool;
+use lilypads::Pond;
 
 #[test]
 fn alloc() {
-  let mut storage = NodePool::new();
-  let idx1 = storage.alloc(42);
-  let idx2 = storage.alloc(123);
+  let mut pool = Pond::new();
+  let idx1 = pool.alloc(42);
+  let idx2 = pool.alloc(123);
 
-  assert_eq!(*storage.get(idx1).unwrap(), 42);
-  assert_eq!(*storage.get(idx2).unwrap(), 123);
+  assert_eq!(*pool.get(idx1).unwrap(), 42);
+  assert_eq!(*pool.get(idx2).unwrap(), 123);
 }
 
 #[test]
 fn get() {
-  let mut storage = NodePool::new();
-  let idx = storage.alloc(42);
+  let mut pool = Pond::new();
+  let idx = pool.alloc(42);
   // Ensure we can access reserved data and can't access free slots
-  assert_eq!(*storage.get(idx).unwrap(), 42);
-  assert_eq!(storage.get(idx + 1), None);
+  assert_eq!(*pool.get(idx).unwrap(), 42);
+  assert_eq!(pool.get(idx + 1), None);
+}
+
+#[test]
+fn mut_get() {
+  let mut pool = Pond::new();
+  let idx = pool.alloc(42);
+  let data2 = pool.get_mut(idx).unwrap();
+  *data2 = 13;
+  assert_eq!(*pool.get(idx).unwrap(), 13);
 }
 
 #[test] 
 fn free() {
-  let mut storage = NodePool::new();
-  let idx = storage.alloc(42);
+  let mut pool = Pond::new();
+  let idx = pool.alloc(42);
   // Was data set?
-  assert_eq!(*storage.get(idx).unwrap(), 42);
-  storage.free(idx);
+  assert_eq!(*pool.get(idx).unwrap(), 42);
+  pool.free(idx);
   // Was data unset?
-  assert_eq!(storage.get(idx), None);
+  assert_eq!(pool.get(idx), None);
   // Ensure we can't double free
-  assert_eq!(storage.free(idx), None);
+  assert_eq!(pool.free(idx), None);
 }
 
 #[test]
 fn write() {
-  let mut storage = NodePool::new();
-  let idx = storage.alloc(42);
+  let mut pool = Pond::new();
+  let idx = pool.alloc(42);
 
-  let old = storage.write(idx, 155).unwrap();
+  let old = pool.write(idx, 155).unwrap();
   // Verify old data was returned and new data is in place
   assert_eq!(old, 42);
-  assert_eq!(*storage.get(idx).unwrap(), 155);
+  assert_eq!(*pool.get(idx).unwrap(), 155);
 
   let idx2 = 13;
-  storage.write(idx2, 29);
+  pool.write(idx2, 29);
   // Ensure the vec was properly resize and the data was marked as reserved
-  assert_eq!(*storage.get(idx2).unwrap(), 29);
+  assert_eq!(*pool.get(idx2).unwrap(), 29);
 }
 
 #[test]
 fn memory_reuse() {
-  let mut storage = NodePool::new();
-  let idx1 = storage.alloc(1);
-  let idx2 = storage.alloc(2);
-  storage.free(idx1);
-  let idx3 = storage.alloc(3);
+  let mut pool = Pond::new();
+  let idx1 = pool.alloc(1);
+  let idx2 = pool.alloc(2);
+  pool.free(idx1);
+  let idx3 = pool.alloc(3);
 
   // Verify reuse
   assert_eq!(idx1, idx3);
   // Verify data
-  assert_eq!(*storage.get(idx2).unwrap(), 2);
-  assert_eq!(*storage.get(idx3).unwrap(), 3);
+  assert_eq!(*pool.get(idx2).unwrap(), 2);
+  assert_eq!(*pool.get(idx3).unwrap(), 3);
 }
 
 #[test]
 fn defrag() {
-  let mut storage = NodePool::new();
-  let mut indices: Vec<_> = (0..5).map(|i| storage.alloc(i) ).collect();
+  let mut pool = Pond::new();
+  let mut indices: Vec<_> = (0..5).map(|i| pool.alloc(i) ).collect();
   // Remove some items to create gaps
-  storage.free(indices[1]).unwrap();
-  storage.free(indices[3]).unwrap();
+  pool.free(indices[1]).unwrap();
+  pool.free(indices[3]).unwrap();
 
   // Defrag and verify remapping
-  let remapped = storage.defrag();
+  let remapped = pool.defrag();
   for (old, new) in remapped.iter() { indices[*old] = *new }
 
   // Verify data is preserved and contiguous
-  assert_eq!(*storage.get(indices[0]).unwrap(), 0);
-  assert_eq!(*storage.get(indices[2]).unwrap(), 2);
-  assert_eq!(*storage.get(indices[4]).unwrap(), 4);
-  assert_eq!(storage.next_allocated(), 3);
+  assert_eq!(*pool.get(indices[0]).unwrap(), 0);
+  assert_eq!(*pool.get(indices[2]).unwrap(), 2);
+  assert_eq!(*pool.get(indices[4]).unwrap(), 4);
+  assert_eq!(pool.next_allocated(), 3);
 }
 
 #[test]
 fn trim_normal() {
-  let mut storage = NodePool::new();
-  let mut indices: Vec<_> = (0..5).map(|i| storage.alloc(i)).collect();
+  let mut pool = Pond::new();
+  let mut indices: Vec<_> = (0..5).map(|i| pool.alloc(i)).collect();
 
   // Remove last two items
-  storage.free(indices[3]).unwrap();
-  storage.free(indices[4]).unwrap();
+  pool.free(indices[3]).unwrap();
+  pool.free(indices[4]).unwrap();
 
   // Trim and verify
-  let remapped = storage.trim();
+  let remapped = pool.trim();
   for (old, new) in remapped.iter() { indices[*old] = *new }
 
   // Verify memory state after trim
-  assert!(matches!(storage.get(2), Some(_)));
-  assert!(matches!(storage.get(3), None));
+  assert!(matches!(pool.get(2), Some(_)));
+  assert!(matches!(pool.get(3), None));
 
   // Verify allocator state after trim
-  assert_eq!(storage.next_allocated(), 3);
+  assert_eq!(pool.next_allocated(), 3);
 
   // Verify remaining data
-  assert_eq!(*storage.get(indices[0]).unwrap(), 0);
-  assert_eq!(*storage.get(indices[1]).unwrap(), 1);
-  assert_eq!(*storage.get(indices[2]).unwrap(), 2);
+  assert_eq!(*pool.get(indices[0]).unwrap(), 0);
+  assert_eq!(*pool.get(indices[1]).unwrap(), 1);
+  assert_eq!(*pool.get(indices[2]).unwrap(), 2);
 }
 
 #[test]
 fn trim_all_free() {
-  let mut storage = NodePool::new();
+  let mut pool = Pond::new();
 
-  let idx1 = storage.alloc(1);
-  let idx2 = storage.alloc(2);
+  let idx1 = pool.alloc(1);
+  let idx2 = pool.alloc(2);
 
   //Set all slots to free
-  storage.free(idx1).unwrap();
-  storage.free(idx2).unwrap();
+  pool.free(idx1).unwrap();
+  pool.free(idx2).unwrap();
 
-  _ = storage.trim();
+  _ = pool.trim();
 
   // Verify memory state
-  assert_eq!(storage.get(0), None);
+  assert_eq!(pool.get(0), None);
 
   // Verify allocator state after trim
-  assert_eq!(storage.next_allocated(), 0);
+  assert_eq!(pool.next_allocated(), 0);
 }
 
 #[test]
 fn trim_empty() {
-  let mut storage = NodePool::<i32>::new();
-  _ = storage.trim();
+  let mut pool = Pond::<i32>::new();
+  _ = pool.trim();
 
   // Verify memory state
-  assert_eq!(storage.get(0), None);
+  assert_eq!(pool.get(0), None);
 
   // Verify allocator state after trim
-  assert_eq!(storage.next_allocated(), 0);
+  assert_eq!(pool.next_allocated(), 0);
 }
 
 #[test]
 fn trim_free() {
-  let mut storage = NodePool::<i32>::new();
-  storage.resize(16);
-  _ = storage.trim();
+  let mut pool = Pond::<i32>::new();
+  pool.resize(16);
+  _ = pool.trim();
   
   // Verify memory state
-  assert_eq!(storage.get(0), None);
+  assert_eq!(pool.get(0), None);
 
   // Verify allocator state after trim
-  assert_eq!(storage.next_allocated(), 0);
+  assert_eq!(pool.next_allocated(), 0);
 }
 
 #[test]
 fn stress() {
   const N: u32 = 1_000_000;
-  let mut storage = NodePool::new();
-  storage.resize(N as usize);
+  let mut pool = Pond::new();
+  pool.resize(N as usize);
 
   // Push a bunch of values into the allocator
-  for i in 0..N { let _ = storage.alloc(i); }
+  for i in 0..N { let _ = pool.alloc(i); }
 }
 
